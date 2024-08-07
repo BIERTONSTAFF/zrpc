115d518519531d884d84222e673921bfc08304e1
Mikhail Matyushev <mikhail.matyushev@gmail.com>
2024-08-07 15:51:11 +0300

    multithreading for stream handling

--- src/server.rs
+++ src/server.rs
@@ -2,6 +2,7 @@ use std::{
     collections::HashMap,
     io::{Read, Write},
     net::TcpListener,
+    process,
     sync::{Arc, Mutex},
 };
 
@@ -33,32 +34,16 @@ impl ZRpcServer {
 
     pub fn start(&mut self) -> Result<(), ()> {
         for res in self.listener.incoming() {
-            match res {
+            let procs = self.procs.clone();
+
+            std::thread::spawn(move || match res {
                 Ok(mut stream) => {
-                    let mut len = [0u8; 4];
-                    stream.read_exact(&mut len).map_err(|_| ())?;
-
-                    let mut buf = vec![0u8; u32::from_be_bytes(len) as usize];
-                    stream.read_exact(&mut buf).map_err(|_| ())?;
-
-                    let req: ZRpcReq = bincode::deserialize(&buf).map_err(|_| ())?;
-                    let bytes = match self.procs.lock().unwrap().get(&req.0) {
-                        Some(proc) => bincode::serialize(&proc(&req.1)).map_err(|_| ())?,
-                        None => bincode::serialize(&ZRpcDt::Error(ErrorKind::ProcedureNotFound))
-                            .map_err(|_| ())?,
-                    };
-
-                    stream
-                        .write_all(&(bytes.len() as u32).to_be_bytes())
-                        .map_err(|_| ())?;
-
-                    println!(
-                        "[ZRpcServer] {} bytes were written",
-                        stream.write(&bytes).map_err(|_| ())?
-                    );
+                    if let Err(e) = Self::handle_stream(&mut stream, &procs) {
+                        eprintln!("Failed to handle stream: {}", e);
+                    }
                 }
-                Err(e) => eprintln!("Failed to handle stream: {}", e),
-            }
+                Err(e) => eprintln!("Failed to accept connection: {}", e),
+            });
         }
         Ok(())
     }
@@ -73,5 +58,42 @@ impl ZRpcServer {
             .lock()
             .unwrap()
             .insert(name.to_string(), Box::new(proc));
+    }
+
+    fn handle_stream(
+        stream: &mut std::net::TcpStream,
+        procs: &Arc<Mutex<HashMap<String, Box<dyn Fn(&Vec<ZRpcDt>) -> ZRpcDt + Send + Sync>>>>,
+    ) -> Result<(), String> {
+        let mut len = [0u8; 4];
+        stream
+            .read_exact(&mut len)
+            .map_err(|_| "Failed to read buffer length")?;
+
+        let mut buf = vec![0u8; u32::from_be_bytes(len) as usize];
+        stream
+            .read_exact(&mut buf)
+            .map_err(|_| "Failed to read buffer")?;
+
+        let req: ZRpcReq =
+            bincode::deserialize(&buf).map_err(|_| "Failed to deserialize buffer")?;
+        let bytes = match procs.lock().unwrap_or_else(|e| e.into_inner()).get(&req.0) {
+            Some(proc) => {
+                bincode::serialize(&proc(&req.1)).map_err(|_| "Failed to serialize result")?
+            }
+            None => bincode::serialize(&ZRpcDt::Error(ErrorKind::ProcedureNotFound))
+                .map_err(|_| "Failed to serialize error")?,
+        };
+
+        stream
+            .write_all(&(bytes.len() as u32).to_be_bytes())
+            .map_err(|_| "Failed to write result length")?;
+
+        println!(
+            "[ZRpcServer:{:?}] {} bytes were written",
+            std::thread::current().id(),
+            stream.write(&bytes).map_err(|_| "Failed to write result")?,
+        );
+
+        Ok(())
     }
 }
